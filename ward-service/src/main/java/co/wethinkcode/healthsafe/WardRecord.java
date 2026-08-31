b93c6bacfb8eaadfd3786e198b684e53b93be774
Nhlanhla Mbokane <nhmboeec025@student.wethinkcode.co.za>
2026-08-31 10:54:55 +0200

    Added jackson dependencies and created WardRecord Java class

---
+++ ward-service/src/main/java/co/wethinkcode/healthsafe/WardRecord.java
@@ -0,0 +1,13 @@
+package co.wethinkcode.healthsafe;
+
+public class WardRecord {
+    public String wardId;
+    public String wing;
+    public String department;
+    public Integer bedsAvailable;
+    public String notes;
+
+    public WardRecord(){
+
+    }
+}
