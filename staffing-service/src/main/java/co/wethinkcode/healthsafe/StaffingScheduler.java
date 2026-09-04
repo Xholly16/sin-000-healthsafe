d5653ae3b5fa1b0897f7842af465242a8c3af4f5
Nhlanhla Mbokane <nhmboeec025@student.wethinkcode.co.za>
2026-09-04 12:40:20 +0200

    Green: implement StaffingScheduler.doctorsRequired for low levels
    Returns 1 doctor for alert level 0-2. mvn test passes: Tests run: 1

---
+++ staffing-service/src/main/java/co/wethinkcode/healthsafe/StaffingScheduler.java
@@ -0,0 +1,11 @@
+package co.wethinkcode.healthsafe;
+
+public class StaffingScheduler {
+
+    public static int doctorsRequired(int alertLevel) {
+        if (alertLevel <= 2){
+            return 1;
+        }
+        return 1;
+    }
+}
